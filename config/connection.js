bb794d8b8158226e7f66d20aa643e505277537f2
Newton <narmstrongv@gmail.com>
2021-07-17 13:54:35 -0700

    fixed .env/login

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('ecommerce_db', 'root', 'AlphaBetaSigma5!', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port:3306,
